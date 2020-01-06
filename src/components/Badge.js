86b2edd09f7b1b304aa5128edc95c72c59c3c24a
William Schnaider Torres Bermon <willtor1100@gmail.com>
2020-01-06 00:48:40 -0500

    Clase: Props

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,37 +1,36 @@
-import React from "react";
+import React from 'react';
+
 import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
-import avatar from '../images/avatar.jpg';
-
 
 class Badge extends React.Component {
-    render() {
-      return (
-        <div className="Badge">
-          <div className="Badge__header">
-            <img src={confLogo} alt="Logo de la conferencia" />
-          </div>
-  
-          <div className="Badge__section-name">
-            <img
-              className="Badge__avatar"
-              src={avatar}
-              alt="Avatar"
-            />
-            <h1>
-              William Schnaider <br /> Torres Bermon
-            </h1>
-          </div>
-  
-          <div className="Badge__section-info">
-            <h3>Software Engineerk</h3>
-            <div>@willtorber</div>
-          </div>
-  
-          <div className="Badge__footer">#platziconf</div>
+  render() {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={confLogo} alt="Logo de la conferencia" />
         </div>
-      );
-    }
+
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src={this.props.avatarUrl}
+            alt="Avatar"
+          />
+          <h1>
+            {this.props.firstName} <br /> {this.props.lastName}
+          </h1>
+        </div>
+
+        <div className="Badge__section-info">
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
+        </div>
+
+        <div className="Badge__footer">#platziconf</div>
+      </div>
+    );
   }
+}
 
 export default Badge;
--- src/index.js
+++ src/index.js
@@ -7,4 +7,13 @@ import Badge from './components/Badge';
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge/>, container);
+ReactDOM.render(
+    <Badge
+      firstName="William"
+      lastName="Torres"
+      avatarUrl="https://www.gravatar.com/avatar?d=identicon"
+      jobTitle="Software Engineer"
+      twitter="willtorber"
+    />,
+    container
+  );
