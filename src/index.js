aeca40d5de951f3e9e619e0a0c9eb178426b23c5
William Schnaider Torres Bermon <willtor1100@gmail.com>
2020-01-06 00:01:09 -0500

    Clase: Nuestro primer componente

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const element = <h1>Hello, Platzi Badges!</h1>;
+import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge/>, container);
