39b398fe691b68bbe3da848afc097e1048ba8f93
William Schnaider Torres Bermon <willtor1100@gmail.com>
2020-01-06 01:31:19 -0500

    Clase: Enlazando eventos

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
-import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
+
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(
-    <Badge
-      firstName="William"
-      lastName="Torres"
-      avatarUrl="https://www.gravatar.com/avatar?d=identicon"
-      jobTitle="Software Engineer"
-      twitter="willtorber"
-    />,
-    container
-  );
+ReactDOM.render(<BadgeNew />, container);
