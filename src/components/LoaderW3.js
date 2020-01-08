d0bcfef80a594dff6c0bb76ef3cb2219a2ad3085
William Schnaider Torres Bermon <willtor1100@gmail.com>
2020-01-08 12:27:44 -0500

    Clase: Mejorando la Experiencia de Usuario durante una petición

---
+++ src/components/LoaderW3.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles/LoaderW3.css';
+
+function LoaderW3(){
+    return <div class="loader"></div> ;
+}
+
+export default LoaderW3;
---
+++ src/components/PageLoading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles/PageLoading.css';
+import LoaderW3 from './LoaderW3';
+
+function PageLoading() {
+  return (
+    <div className="PageLoading">
+      <LoaderW3 />
+    </div>
+  );
+}
+
+export default PageLoading;
